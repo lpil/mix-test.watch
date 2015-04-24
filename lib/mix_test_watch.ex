670beb5977625276ec7c76078ba4e3bdf3e20d74
Louis Pilfold <louis@lpil.uk>
2015-04-24 22:33:05 +0100

    Debounce info messages

--- lib/mix_test_watch.ex
+++ lib/mix_test_watch.ex
@@ -3,6 +3,9 @@ defmodule Mix.Tasks.Test.Watch do
   use GenServer
 
   def run(_) do
+    {:ok, agent} = Agent.start_link(
+      fn -> true end, name: :mix_test_watch_state
+    )
     Application.start :fs
     GenServer.start_link( __MODULE__, [], name: __MODULE__ )
     :timer.sleep :infinity
@@ -16,19 +19,23 @@ defmodule Mix.Tasks.Test.Watch do
 
   def handle_info({_pid, {:fs, :file_event}, {path, _event}}, _) do
     path = to_string path
-    if watching? path do run_tests end
+    if watching?(path) && not_running_tests? do run_tests end
     {:noreply, []}
   end
 
+  defp not_running_tests? do
+    Agent.get( :mix_test_watch_state, fn x -> x end )
+  end
 
   defp watching?(path) do
     Regex.match?( ~r/\.exs?\z/i, path )
   end
 
   defp run_tests do
-    # Mix.Tasks.Test.run []
-    output = :os.cmd( mix_cmd )
-    IO.puts to_string(output)
+    IO.puts "Running tests..."
+    Agent.update( :mix_test_watch_state, fn _ -> false end )
+    IO.puts( to_string :os.cmd(mix_cmd) )
+    Agent.update( :mix_test_watch_state, fn _ -> true end )
   end
 
   defp mix_cmd do

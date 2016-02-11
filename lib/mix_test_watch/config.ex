522a26e6327bfb560b0de29473f8a19f3aab124d
Louis Pilfold <louis@lpil.uk>
2016-02-11 20:48:24 +0000

    Create config struct

---
+++ lib/mix_test_watch/config.ex
@@ -0,0 +1,35 @@
+defmodule MixTestWatch.Config do
+  @moduledoc """
+  Responsible for gathering and packaging the configuration for the task.
+  """
+
+  @default_tasks ~w(test)
+  @default_prefix "mix"
+
+  defstruct tasks:    @default_tasks,
+            prefix:   @default_prefix,
+            cli_args: ""
+
+
+  @spec new([String.t]) :: %__MODULE__{}
+  @doc """
+  Create a new config struct, taking values from the ENV
+  """
+  def new(cli_args \\ []) do
+    args = Enum.join(cli_args, " ")
+    %__MODULE__{
+      tasks:    get_tasks(),
+      prefix:   get_prefix(),
+      cli_args: args,
+    }
+  end
+
+
+  defp get_tasks do
+    Application.get_env(:mix_test_watch, :tasks, @default_tasks)
+  end
+
+  defp get_prefix do
+    Application.get_env(:mix_test_watch, :prefix, @default_prefix)
+  end
+end
---
+++ test/mix_test_watch/config_test.exs
@@ -0,0 +1,45 @@
+defmodule MixTestWatch.ConfigTest do
+  use ExUnit.Case, async: false
+  use TemporaryEnv
+
+  alias MixTestWatch.Config
+
+  test "new/1 takes :tasks from the env" do
+    TemporaryEnv.set :mix_test_watch, tasks: :env_tasks do
+      config = Config.new
+      assert config.tasks == :env_tasks
+    end
+  end
+
+  test ~s(new/1 defaults :tasks to ["test"]) do
+    TemporaryEnv.delete :mix_test_watch, :tasks do
+      config = Config.new
+      assert config.tasks == ["test"]
+    end
+  end
+
+
+  test "new/1 takes :prefix from the env" do
+    TemporaryEnv.set :mix_test_watch, prefix: :env_prefix do
+      config = Config.new
+      assert config.prefix == :env_prefix
+    end
+  end
+
+  test ~s(new/1 defaults :prefix to "mix") do
+    TemporaryEnv.delete :mix_test_watch, :prefix do
+      config = Config.new
+      assert config.prefix == "mix"
+    end
+  end
+
+  test "new/1 joins cli_args into a string" do
+    config = Config.new(["hello", "world"])
+    assert config.cli_args == "hello world"
+  end
+
+  test "new/1 default cli_args to empty string" do
+    config = Config.new
+    assert config.cli_args == ""
+  end
+end

f2c62fe53a5a1ab72f6d02dde2efbf01cdffc6e0
Niklas Hofer <niklas+dev@lanpartei.de>
2016-02-16 20:35:19 +0100

    use our testfile as mock for itself

--- test/mix_test_watch/files_test.exs
+++ test/mix_test_watch/files_test.exs
@@ -1,34 +1,30 @@
 defmodule MixTestWatch.FilesTest do
   use ExUnit.Case
-  import MixTestWatch.MockFsHelper
 
   import MixTestWatch.Files, only: [find_test: 1]
 
+  # The module in test actually checks whether files exist to find the right
+  # one to run. We use ourselves as a fixture to avoid having to create a mock
+  # file system, mock system methods or carry around a "fake" file system just
+  # for tests.
+
+  # We assume this test is run from the project's root directory
+  @this_file Path.relative_to(__ENV__.file, System.cwd)
+
   test "test files are responsible for themselved" do
-    tfile = "test/foo/bar_test.exs"
-    with_mock_fs [tfile] do
-      assert fs(tfile) == find_test(fs tfile)
-    end
+    assert @this_file == find_test(@this_file)
   end
 
   test "finds test file for implementation in lib/" do
-    tfile = "test/bar/foo_test.exs"
-    with_mock_fs [tfile] do
-      assert fs(tfile) == find_test(fs "lib/bar/foo.ex")
-    end
+    assert @this_file == find_test("lib/mix_test_watch/files.ex")
   end
 
   test "finds test file for implementation in web/ (for phoenix)" do
-    tfile = "test/models/plant_test.exs"
-    with_mock_fs [tfile] do
-      assert fs(tfile) == find_test(fs "web/models/plant.ex")
-    end
+    assert @this_file == find_test("web/mix_test_watch/files.ex")
   end
 
   test "finds nothing when test file does not exist" do
-    with_mock_fs [] do
-      assert nil == find_test(fs "lib/bar/foo.ex")
-    end
+    assert nil == find_test("lib/mix_test_watch/rummelpummel.ex")
   end
 
 end

ae61ee0368fe0db2c95a5e438566eb1d3af1a473
Louis Pilfold <louis@lpil.uk>
2016-05-03 16:39:16 +0100

    Move dep task list calculation to compile time

--- lib/mix_test_watch/compile_task.ex
+++ lib/mix_test_watch/compile_task.ex
@@ -3,14 +3,15 @@ defmodule MixTestWatch.CompileTask do
   Run the compile task, even if it has been run before.
   """
 
-  def run do
-    Enum.each(dep_tasks, &Mix.Task.reenable/1)
-    Mix.Task.run("compile", [])
-  end
-
-  def dep_tasks do
-    tasks = ~w(compile compile.all compile.protocols loadpaths deps.loadpaths)
+  tasks = ~w(compile compile.all compile.protocols loadpaths deps.loadpaths)
+  dep_tasks =
     Mix.Tasks.Compile.compilers()
     |> Enum.reduce(tasks, fn(x, acc)-> ["compile.#{x}"|acc] end)
+
+  @dep_tasks dep_tasks
+
+  def run do
+    Enum.each(@dep_tasks, &Mix.Task.reenable/1)
+    Mix.Task.run("compile", [])
   end
 end

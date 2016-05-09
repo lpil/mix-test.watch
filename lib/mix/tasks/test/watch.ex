a317d57132e7660dd0ff77a2166636645593bd9a
Louis Pilfold <louis@lpil.uk>
2016-05-09 15:46:35 +0100

    Set preferred mix task CLI env

--- lib/mix/tasks/test/watch.ex
+++ lib/mix/tasks/test/watch.ex
@@ -1,12 +1,11 @@
 defmodule Mix.Tasks.Test.Watch do
   use Mix.Task
 
-  @shortdoc """
-  Automatically run tests on file changes
-  """
   @moduledoc """
   A task for running tests whenever source files change.
   """
+  @shortdoc "Automatically run tests on file changes"
+  @preferred_cli_env :test
 
   defdelegate run(args), to: MixTestWatch
 end

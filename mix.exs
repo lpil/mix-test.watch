8ee5c331059e821830a325cd59e87821b3434f88
Daniel P. Shannon <daniel@daniel.sh>
2017-12-08 22:48:37 -0500

    Bump FS dependency.

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule MixTestWatch.Mixfile do
 
   defp deps do
     [# File system event watcher
-     {:fs, "~> 0.9.1"},
+     {:fs, "~> 2.12"},
      # Style linter
      {:dogma, "~> 0.1", only: ~w(dev test)a},
      # App env state test helper

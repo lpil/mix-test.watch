dde68621652d163e46bd20ffffe5f66ed1bcf7a1
Louis Pilfold <louis@lpil.uk>
2018-07-30 21:20:36 +0100

    Fix tests broken by dep update

--- test/mix_test_watch/config_test.exs
+++ test/mix_test_watch/config_test.exs
@@ -5,7 +5,7 @@ defmodule MixTestWatch.ConfigTest do
   alias MixTestWatch.Config
 
   test "new/1 takes :tasks from the env" do
-    TemporaryEnv.set :mix_test_watch, tasks: :env_tasks do
+    TemporaryEnv.put :mix_test_watch, :tasks, :env_tasks do
       config = Config.new()
       assert config.tasks == :env_tasks
     end
@@ -19,7 +19,7 @@ defmodule MixTestWatch.ConfigTest do
   end
 
   test "new/1 takes :exclude from the env" do
-    TemporaryEnv.set :mix_test_watch, exclude: [~r/migration_.*/] do
+    TemporaryEnv.put :mix_test_watch, :exclude, [~r/migration_.*/] do
       config = Config.new()
       assert config.exclude == [~r/migration_.*/]
     end
@@ -37,7 +37,7 @@ defmodule MixTestWatch.ConfigTest do
   end
 
   test "new/1 takes :extra_extensions from the env" do
-    TemporaryEnv.set :mix_test_watch, extra_extensions: [".haml"] do
+    TemporaryEnv.put :mix_test_watch, :extra_extensions, [".haml"] do
       config = Config.new()
       assert config.extra_extensions == [".haml"]
     end
@@ -54,21 +54,21 @@ defmodule MixTestWatch.ConfigTest do
   end
 
   test "new/1 takes :clear from the env" do
-    TemporaryEnv.set :mix_test_watch, clear: true do
+    TemporaryEnv.put :mix_test_watch, :clear, true do
       config = Config.new()
       assert config.clear
     end
   end
 
   test "new/1 takes :timestamp from the env" do
-    TemporaryEnv.set :mix_test_watch, timestamp: true do
+    TemporaryEnv.put :mix_test_watch, :timestamp, true do
       config = Config.new()
       assert config.timestamp
     end
   end
 
   test "new/1 takes :shell_prefix from the env" do
-    TemporaryEnv.set :mix_test_watch, cli_executable: "iex -S" do
+    TemporaryEnv.put :mix_test_watch, :cli_executable, "iex -S" do
       config = Config.new()
       assert config.cli_executable == "iex -S"
     end

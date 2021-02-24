70f89345a9f6d2f200eb8cc2971d9315eb161db2
Kian-Meng Ang <kianmeng@cpan.org>
2021-02-24 21:18:40 +0800

    Misc doc changes
    Besides other changes, this commit ensures the generated HTML doc for
    HexDocs.pm will become the main source doc for this Elixir library which
    leverage on ExDoc features.
    
    List of changes:
    * Fix markdowns
    * Set readme as main HTML page
    * Add changelog to HTML page
    * Tally minimum Elixir version with Travis CI
    * Update license year
    * Use common source url
    * Add source reference
    * Use and set latest ex_doc
    * Badges and more badges!
    * Revise content
    * Update gitignore

--- mix.exs
+++ mix.exs
@@ -1,39 +1,56 @@
 defmodule MixTestWatch.Mixfile do
   use Mix.Project
 
+  @source_url "https://github.com/lpil/mix-test.watch"
   @version "1.0.2"
 
   def project do
     [
       app: :mix_test_watch,
       version: @version,
-      elixir: "~> 1.0",
+      elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
       name: "mix test.watch",
       description: "Automatically run tests when files change",
       package: [
         maintainers: ["Louis Pilfold"],
         licenses: ["MIT"],
-        links: %{"GitHub" => "https://github.com/lpil/mix-test.watch"},
+        links: %{
+          "Changelog" => "https://hexdocs.pm/mix_test_watch/changelog.html",
+          "GitHub" => @source_url
+        },
         files: ~w(LICENCE README.md CHANGELOG.md lib priv mix.exs)
       ]
     ]
   end
 
   def application do
-    [mod: {MixTestWatch, []}, applications: [:file_system]]
+    [
+      mod: {MixTestWatch, []},
+      applications: [:file_system]
+    ]
   end
 
   defp deps do
-    # File system event watcher
     [
+      # File system event watcher
       {:file_system, "~> 0.2.1 or ~> 0.3"},
       # App env state test helper
       {:temporary_env, "~> 2.0", only: :test},
       # Documentation generator
-      {:ex_doc, ">= 0.12.0", only: :dev}
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["CHANGELOG.md", "README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}"
     ]
   end
 end
